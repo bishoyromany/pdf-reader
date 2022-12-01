4ef2b79c29a161f1bdd70c8bbc7eaf3af6f45a6f
Bishoy Romany <bishoy@queensenglish.co>
2022-12-01 18:21:53 +0200

    Update NExt cors

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -1,5 +1,14 @@
+import NextCors from 'nextjs-cors';
+
 export default async function handler(req, res) {
     const { url } = req.query;
+
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
 
     try {
         const response = await fetch(
