c32bef45c6460193f98261c3e0e8933635a38a29
Bishoy Romany <bishoy@queensenglish.co>
2022-12-01 18:17:04 +0200

    stash

--- pdfjs/web/viewer.js
+++ pdfjs/web/viewer.js
@@ -743,7 +743,7 @@
       };
       {
         defaultOptions.defaultUrl = {
-          value: "https://be-pdf-reader.vercel.app/api/pdf" + window.location.search,
+          value: window.location.search.replace('?url=', '').length > 10 ? "https://be-pdf-reader.vercel.app/api/pdf" + window.location.search : "./pdfjs/web/compressed.tracemonkey-pldi-09.pdf",
           kind: OptionKind.VIEWER
         };
         defaultOptions.disablePreferences = {
